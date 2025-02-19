285b7188cf1fa7557985f6a7eea791978af820db
weizhang-2706 <wei2001ca@yahoo.com>
2025-02-18 20:36:44 -0500

    use dummy auto cmd for ControlBox

--- src/main/java/frc/robot/robotcontainers/ControlBoxContainer.java
+++ src/main/java/frc/robot/robotcontainers/ControlBoxContainer.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.lib.lib2706.TunableNumber;
@@ -80,9 +81,6 @@ public class ControlBoxContainer extends RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    int autoId = m_autoSelector.getAutoId();
-    System.out.println("*********************** Auto Id"+autoId);
-
-    return m_autoRoutines.getAutonomousCommand(autoId);
+    return new InstantCommand();
   }
 }
