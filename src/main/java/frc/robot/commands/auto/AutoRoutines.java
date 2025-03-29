35638f9f5ce8e60940ae453943d29b477a5681f5
Selina Yunfei <selinayunfei@gmail.com>
2025-03-28 20:02:34 -0400

    fixed reset command

--- src/main/java/frc/robot/commands/auto/AutoRoutines.java
+++ src/main/java/frc/robot/commands/auto/AutoRoutines.java
@@ -83,10 +83,7 @@ public class AutoRoutines extends SubsystemBase {
          NamedCommands.registerCommand("elevatorIntake",new SetElevator(Config.ElevatorSetPoints.FEEDER));
         // NamedCommands.registerCommand("coralIntake", new CoralIntake(-0.3,  0.3).withTimeout(1.5));
          NamedCommands.registerCommand("CoralScore", new CoralDepositorCommand(true, false).withTimeout(2));
-         NamedCommands.registerCommand("reset", 
-         new SelectByAllianceCommand(
-             PhotonSubsystem.getInstance().getResetCommand(),
-             PhotonSubsystem.getInstance().getResetCommand()));
+         NamedCommands.registerCommand("reset", PhotonSubsystem.getInstance().getResetCommand());
          NamedCommands.registerCommand("vision-move",new PhotonMoveToTarget(false, false, false));
     }
 
